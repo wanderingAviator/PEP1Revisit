e25f2373f960f30e47bc6736e843da52dc497de8
marco3dog <62681327+marco3dog@users.noreply.github.com>
2023-08-07 13:00:41 -0700

    added more apis and endpoints

--- product_api.py
+++ product_api.py
@@ -1,16 +1,21 @@
+from flask import json
 from my_mongo_connection import product
 from datetime import datetime
+from bson import json_util
+
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
 
 #create product
 def create_product(productObject):
     now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     product.insert_one({
-        "product_name" : productObject.name,
-        "product_desc" : productObject.desc,
-        "in_stock" : productObject.in_stock,
-        "product_price" : productObject.price,
-        "product_category" : productObject.category,
-        "product_brand" : productObject.brand,
+        "product_name" : productObject['product_name'],
+        "product_desc" : productObject['product_desc'],
+        "in_stock" : productObject['in_stock'],
+        "product_price" : productObject['product_price'],
+        "product_category" : productObject['product_category'],
+        "product_brand" : productObject['product_brand'],
         "updated_at" : now
     })
 
@@ -35,7 +40,11 @@ def find_by_id(id):
 
 #find by name
 def find_by_name(name):
-    return product.find_one({"product_name" : name})
+    return parse_json(product.find_one({"product_name" : name}))
+
+#get all
+def get_all_products():
+    return product.find({})
 
 #delete
 def delete_product(id):
