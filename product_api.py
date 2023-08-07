cc0977346ff07de27775bb3e9f5a60fd019010fe
marco3dog <62681327+marco3dog@users.noreply.github.com>
2023-08-07 09:55:04 -0700

    added more product apis

--- product_api.py
+++ product_api.py
@@ -13,7 +13,32 @@ def create_product(productObject):
         "product_brand" : productObject.brand,
         "updated_at" : now
     })
-#update product
 
-#find by id
+#update product
+def update_product(id, productObject):
+    now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    product.update_one({
+        {"_id" : id},{
+        "product_name" : productObject.name,
+        "product_desc" : productObject.desc,
+        "in_stock" : productObject.in_stock,
+        "product_price" : productObject.price,
+        "product_category" : productObject.category,
+        "product_brand" : productObject.brand,
+        "updated_at" : now
+        }
+    })
+
+#find by id 
+def find_by_id(id):
+    return product.find_one({"_id" : id})
+
+#find by name
+def find_by_name(name):
+    return product.find_one({"product_name" : name})
+
+#delete
+def delete_product(id):
+    product.delete_one({"_id" : id})
+
 
