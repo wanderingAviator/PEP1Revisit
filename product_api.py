3c185ee2d2082ed1313f235516350a80e16a74ad
marco3dog <62681327+marco3dog@users.noreply.github.com>
2023-08-07 09:20:23 -0700

    created mongo_connection and started product api

---
+++ product_api.py
@@ -0,0 +1,19 @@
+from my_mongo_connection import product
+from datetime import datetime
+
+#create product
+def create_product(productObject):
+    now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    product.insert_one({
+        "product_name" : productObject.name,
+        "product_desc" : productObject.desc,
+        "in_stock" : productObject.in_stock,
+        "product_price" : productObject.price,
+        "product_category" : productObject.category,
+        "product_brand" : productObject.brand,
+        "updated_at" : now
+    })
+#update product
+
+#find by id
+
