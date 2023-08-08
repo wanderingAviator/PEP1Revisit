d429ae2b56cf04792586db93dd22b40b3828021e
marco3dog <62681327+marco3dog@users.noreply.github.com>
2023-08-08 07:55:34 -0700

    added post product api

--- app_mongo.py
+++ app_mongo.py
@@ -1,10 +1,26 @@
 from datetime import datetime
 from flask import Flask, request, jsonify, render_template, redirect, url_for, session
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from product_api import create_product, find_by_name
 
 app = Flask(__name__)
 api = Api(app)
+
+product_post_args = reqparse.RequestParser()
+product_post_args.add_argument("product_name", type=str, help="Name is required", required=True)
+product_post_args.add_argument("product_desc", type=str, help="Description is required", required=True)
+product_post_args.add_argument("in_stock", type=bool, help="In Stock is required", required=True)
+product_post_args.add_argument("product_price", type=str, help="Price is required", required=True)
+product_post_args.add_argument("product_category", type=str, help="Category is required", required=True)
+product_post_args.add_argument("product_brand", type=str, help="Brand is required", required=True)
+
+product_update_args = reqparse.RequestParser()
+product_update_args.add_argument("product_name", type=str)
+product_update_args.add_argument("product_desc", type=str)
+product_update_args.add_argument("in_stock", type=bool)
+product_update_args.add_argument("product_price", type=str)
+product_update_args.add_argument("product_category", type=str)
+product_update_args.add_argument("product_brand", type=str)
 
 air_jordan = {
     "product_name" : "Air Jordan",
@@ -15,16 +31,14 @@ air_jordan = {
     "product_brand" : "Nike"
 }
 
-@app.route('/product', methods=['POST'])
-def post_product(productObject):
-    create_product(productObject)
-
-#post_product(air_jordan)
-#print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 
 class Product(Resource):
     def get(self, name):
         return find_by_name(name)
+    
+    def post(self, name):
+        args = product_post_args.parse_args()
+        create_product(args)
     
 
 print(find_by_name("Air Jordan")) 
