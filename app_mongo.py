b7057c90a9ea907a01ae471948cf3d382bb1f4fa
Jesus Ramos <jesusramos.jr1999@gmail.com>
2023-08-08 13:31:08 -0500

    Changed the review endpoints so that we can look at reviews given a product id

--- app_mongo.py
+++ app_mongo.py
@@ -37,6 +37,8 @@ product_update_args.add_argument("product_brand", type=str)
 
 #for review
 review_parser = reqparse.RequestParser()
+review_parser.add_argument("customer_id", type=float, help="Customer id is required", required=True)
+review_parser.add_argument("product_id", type=float, help="Product id is required", required=True)
 review_parser.add_argument("rating", type=float, help="In Stock is required", required=True)
 review_parser.add_argument("comment", type=str, help="Comment is required", required=True)
 
@@ -67,10 +69,21 @@ class Product(Resource):
         product_api.create_product(args)
 
 
+class User(Resource):
+    def post(self, username):
+         args = user_post_args.parse_args()
+         user_api.create_user(args) 
+
+    def get(self, username):
+        return user_api.find_by_username(username)  
+
+
+class ProductReviewsResource(Resource):
+    def get(self, product_id):
+            return review_api.find_by_product(product_id), 200
+
+
 class Review(Resource):
-    # def get_all(self):
-    #     return review_api.find_all(), 200
-    
     def get(self, review_id):
         # Convert the string item_id to ObjectId
         object_id = ObjectId(review_id)
@@ -80,24 +93,11 @@ class Review(Resource):
         else:
             return {"message": "Review not found"}, 404
     
-    # def get_by_product(self, product_id):
-    #     return review_api.find_by_product(product_id), 200
-    
     # def get_by_customer(self, customer_id):
     #     return review_api.find_by_customer(customer_id), 200
     
     def put(self, review_id):
         args = review_parser.parse_args()
-        
-class User(Resource):
-    def post(self, username):
-         args = user_post_args.parse_args()
-         user_api.create_user(args) 
-
-    def get(self, username):
-        return user_api.find_by_username(username)   
-
-
         # Convert the string item_id to ObjectId
         object_id = ObjectId(review_id)
 
@@ -121,10 +121,11 @@ class User(Resource):
         else:
             return {"message": "Review not found"}, 404
 
-    
+
 print(product_api.find_by_name("Air Jordan")) 
 api.add_resource(Product, '/product/<name>')
-api.add_resource(Review, '/review/<string:review_id>', '/reviews', '/review/customer/<int:customer_id>', '/review/product/<int:product_id>')
+api.add_resource(Review, '/review/<string:review_id>', '/review')
+api.add_resource(ProductReviewsResource, '/review/product/<int:product_id>')
 api.add_resource(User, '/user/<username>')
 
 #USER
